c73eba05bcf1d424f5340a0cb14c3e2cf403e4f5
Gökhan Uysal <gokhanuysal2001@gmail.com>
2021-12-24 15:36:39 +0300

    Launch page overview done

--- src/Starter/StartFrame.java
+++ src/Starter/StartFrame.java
@@ -1,6 +1,7 @@
 package Starter;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -18,20 +19,22 @@ import javax.swing.JPanel;
 public class StartFrame extends JFrame implements ActionListener{
 	private String title;
 	private int[] size;
-	private JLayeredPane middlePane;
-	private JLabel middleLabel;
-	private JLabel backgroundLabel;
+	private JPanel centerPanel;
+	private JLabel mainTittle;
 	private JButton customer;
 	private JButton manager;
 	private ImageIcon backgroundImg;
 	
 	public StartFrame() {
-		backgroundImg = new ImageIcon("Picture/RestaurantInterior2.jpeg");
-		middlePane = new JLayeredPane();
-		backgroundLabel = new JLabel();
-		middleLabel = new JLabel();
-		customer = new JButton();
-		manager = new JButton();
+		backgroundImg = new ImageIcon();
+		centerPanel = new JPanel();
+		mainTittle = new JLabel();
+		customer = new JButton("Customer");
+		manager = new JButton("Manager");
+		
+		customer.addActionListener(this);
+		manager.addActionListener(this);
+		
 		
 		
 		size = new int[2];
@@ -39,44 +42,58 @@ public class StartFrame extends JFrame implements ActionListener{
 		size[1] = 720;
 		title = "Welcome to St. Regis Brasserie";
 		
+		
+		centerPanel();
+		
 		this.setTitle(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(size[0] , size[1]);
-		this.setLayout(new FlowLayout(FlowLayout.CENTER));
-		this.getContentPane().setBackground(Color.cyan);
+		this.getContentPane().setBackground(Color.LIGHT_GRAY);
+		this.setLayout(new FlowLayout(FlowLayout.CENTER , 250 , 275));
+		this.setResizable(false);
+		//this.pack();
 		this.setVisible(true);	
+	}
+	
+	public void centerPanel() {
+		centerPanel.setOpaque(true);
+		centerPanel.setPreferredSize(new Dimension(500 , 150));
+		centerPanel.setBackground(this.getBackground().getColor(title));
+		centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER , 10 , 20));
+		mainTittle();
+		Buttons();
+		this.add(centerPanel);
+	
+	}
+	
+	public void mainTittle() {
+		mainTittle.setText("Welcome to the\nSr. Regis Brasserie");
+		mainTittle.setFont(new Font("Go Mono for Powerline" , Font.ITALIC , 20));
+		mainTittle.setForeground(Color.black);
+		centerPanel.add(mainTittle);
+	}
+	
+	public void Buttons() {
+		customer.setPreferredSize(new Dimension(100 , 40));
+		manager.setPreferredSize(new Dimension(100 , 40));
+		customer.setFocusable(false);
+		manager.setFocusable(false);
 		
-		pane();
-	}
-	
-	public void pane() {
-		middlePane.setSize(new Dimension(800 , 800));
-		bacground();
-		middleScreen();
-		this.add(middlePane);
-	}
-	
-	public void bacground() {
-		backgroundLabel.setIcon(backgroundImg);
-		backgroundLabel.setBackground(Color.LIGHT_GRAY);
-		//backgroundLabel.setPreferredSize(new Dimension(200 , 200));
-		backgroundLabel.setBounds(0 , 0  , 500 , 500);
-		backgroundLabel.setOpaque(true);
-		middlePane.add(backgroundLabel);
-	}
-	
-	public void middleScreen() {
-		middleLabel.setText(title);
-		middleLabel.setFont(new Font("MW Boli" , Font.ITALIC , 50));
-		middleLabel.setBounds(0 , 0  , 200 , 200);
-		middleLabel.setOpaque(true);
-		middlePane.add(middleLabel);
+		centerPanel.add(customer);
+		centerPanel.add(manager);
+		
+		customer.setAlignmentY(CENTER_ALIGNMENT);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getSource() == customer) {
+			dispose();
+		}
+		else if (e.getSource() == manager) {
+			dispose();
+		}
 	}
 
 }
