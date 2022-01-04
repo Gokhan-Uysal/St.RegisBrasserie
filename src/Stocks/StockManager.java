8af820ccb63afc7d7fc0c3473031b37f704fbe19
Gökhan Uysal <gokhanuysal2001@gmail.com>
2022-01-04 17:46:13 +0300

    Launch page overview done

--- src/Stocks/StockManager.java
+++ src/Stocks/StockManager.java
@@ -8,11 +8,13 @@ public class StockManager {
 
 	
 	public static void connection() throws SQLException {
-		String url = "jdbc:postgresql://localhost:5432/FoodStocks";
+		//String url = "jdbc:postgresql:///stock-manager?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=	\n"
+				//+ "restaurant-manager-337114:europe-west4:stock-manager:stock-manager";
+		String url = "jdbc:postgresql://localhost:5432/stock-manager";
 		Connection connection;
 		
 		try {
-			connection = DriverManager.getConnection(url , "postgres" , "Golden05061881!");
+			connection = DriverManager.getConnection(url , "guysal" , "1029384756!");
 			System.out.println("Connected!");
 		} 
 		catch (SQLException e) {
