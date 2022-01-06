929ebc31257d73284c08bc17e9af2f40ac84394b
Gökhan Uysal <gokhanuysal2001@gmail.com>
2022-01-06 15:02:38 +0300

    Launch page overview done

--- src/main.java
+++ src/main.java
@@ -10,6 +10,14 @@ import Starter.StartFrame;
 import Stocks.DbManager;
 import Stocks.StockManager;
 
+/* *********** Pledge of Honor ***************************************************************************
+ * * * * * * * * *
+I hereby certify that I have completed this programming project on my own
+without any help from anyone else. The effort in the project thus belongs
+completely to me. I did not search for a solution, or I did not consult to any program written by other students or did not copy any program from other sources. I read and followed the guidelines provided in the project description.
+READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID SIGNATURE: <Gökhan Uysal, 76030>
+************************************************************************************************************/
+
 public class main {
 	
 	public static void main(String[] args) {
