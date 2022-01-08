e75b3316264a0ec6b0055f78fdfe8878f442cbab
GokhanUysal <GokhanUysal@CommanderRex>
2022-01-08 21:23:44 +0300

    Final Commit

--- src/main.java
+++ src/main.java
@@ -15,7 +15,7 @@ import Stocks.StockManager;
 I hereby certify that I have completed this programming project on my own
 without any help from anyone else. The effort in the project thus belongs
 completely to me. I did not search for a solution, or I did not consult to any program written by other students or did not copy any program from other sources. I read and followed the guidelines provided in the project description.
-READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID SIGNATURE: <Gökhan Uysal, 76030>
+READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID SIGNATURE: <Gokhan Uysal, 76030>
 ************************************************************************************************************/
 
 public class main {
